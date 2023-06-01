3b18b7465f5d0ce8193b1e954d2c7e0a5d1499e7
waswadennis@gmail.com <waswadennis@gmail.com>
2023-06-01 11:15:39 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,23 @@
+SELECT 
+  -- from raw orders
+  o.orderid, 
+  o.orderidate,
+  o.shipdate,
+  o.shipmode,
+  o.ordersellingprice - o.ordercostprice AS orderprofit,
+  o.ordersellingprice,
+  o.ordercostprice,
+  -- from raw customer 
+  c.customername,
+  c.segment, 
+  c.country,
+  -- from raw product
+  p.category,
+  p.productname,
+  p.subcategory
+
+FROM {{ ref('raw_orders') }} AS o
+LEFT JOIN {{ ref('raw_customer') }} AS c
+  ON o.customerid = c.customerid
+LEFT JOIN {{ ref('raw_product') }} AS p
+  ON o.productid = p.productid
