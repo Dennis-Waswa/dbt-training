be11cafb8696088bb8c51106b37ae6b6170b1dc0
waswadennis@gmail.com <waswadennis@gmail.com>
2023-06-02 12:55:44 +0000

    added deliver tam seed and added it to stg_orders

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 SELECT 
   -- from raw orders
+  {{ dbt_utils.surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders,
   o.orderid, 
   o.orderidate,
   o.shipdate,
@@ -18,11 +19,13 @@ SELECT
   p.productname,
   p.subcategory,
   {{markup('ordersellingprice', 'ordercostprice') }} as markup
-
+  d.delivery_team
 FROM {{ ref('raw_orders') }} AS o
 LEFT JOIN {{ ref('raw_customer') }} AS c
   ON o.customerid = c.customerid
 LEFT JOIN {{ ref('raw_product') }} AS p
   ON o.productid = p.productid
+left join {{ref('delivery_team')}} as d
+on o.shipmode = d.shipmode
 
 --{{limit_data_in_dev('orderdate')}}
